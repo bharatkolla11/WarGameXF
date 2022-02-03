98a7d8ff4286bc903869697fe2029ef463ed4821
bharatchowdarykolla <bharatkolla11@gmail.com>
2022-02-02 22:14:16 -0500

    Added Missing Styles

---
+++ WarGameXam/Styles/GeneralDeviceStyles.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace WarGameXam.Styles
+{
+    public partial class GeneralDeviceStyles : ResourceDictionary
+    {
+        public static GeneralDeviceStyles SharedInstance { get; } = new GeneralDeviceStyles();
+        public GeneralDeviceStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WarGameXam/Styles/SmallDeviceStyles.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace WarGameXam.Styles
+{
+    public partial class SmallDeviceStyles : ResourceDictionary
+    {
+        public static SmallDeviceStyles SharedInstance { get; } = new SmallDeviceStyles();
+        public SmallDeviceStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
